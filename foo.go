f0d24ab830b28731d9fe64bdbd80d426738b6fd1
motohiro-nakamura <motohiro.nakamura@dena.com>
2020-06-30 01:09:57 +0900

    add exported comment

--- foo.go
+++ foo.go
@@ -2,14 +2,17 @@ package main
 
 import "fmt"
 
+// Foo
 type Foo struct {
 	Message string
 }
 
+// NewFoo
 func NewFoo(message string) *Foo {
 	return &Foo{Message: message}
 }
 
+// Greet
 func (f *Foo) Greet() string {
 	return fmt.Sprintf("Hello %s", f.Message)
 }
